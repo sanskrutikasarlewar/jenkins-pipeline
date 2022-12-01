77da71a4f8bfb4c96fa4853549ceb14ceeec3208
anjali <anjali@gmail.com>
2022-12-01 15:27:00 +0530

    final code to commit

--- build.groovy
+++ build.groovy
@@ -17,7 +17,7 @@ pipeline {
                      sh 'sudo apt update -y'
                      sh 'sudo apt install awscli -y'
                      sh 'aws s3 ls'
-                    // sh 'aws s3 mb s3://chorbucket-builder --region us-west-2' 
+                     sh 'aws s3 mb s3://chorbucket-builder --region us-west-2' 
                      sh 'sudo mv /var/lib/jenkins/workspace/maven-build/target/studentapp-2.2-SNAPSHOT.war /tmp/student-${BUILD_ID}.war'
                      sh 'aws s3 cp /tmp/student-${BUILD_ID}.war  s3://chorbucket-builder'   
                 }
