375c158aea1759384dd355041f28919da6fa8f98
anjali <anjali@gmail.com>
2022-12-01 14:25:11 +0530

    code completed final

--- build.groovy
+++ build.groovy
@@ -11,15 +11,17 @@ pipeline {
                 sh 'mvn clean package' 
             }
         }
-        stage('artifact to s3-bucket') {
+        stage('artifact to s3') {
             steps {
-                withAWS(credentials: 'mohit', region: 'us-west-2') {
-                sh 'sudo apt update -y'
-                sh 'sudo apt install awscli -y'
-                sh 'sudo s3 ls'
-                sh 'aws s3 mb s3://chorbucket-builder --region us-west-2'        
-            }
-            }
+                 withAWS(credentials: 'mohit', region: 'us-west-2') {
+                     sh 'sudo apt update -y'
+                     sh 'sudo apt install awscli -y'
+                     sh 'aws s3 ls'
+                    // sh 'aws s3 mb s3://chorbucket-builder --region us-west-2' 
+                     sh 'sudo mv /var/lib/jenkins/workspace/maven-build/target/studentapp-2.2-SNAPSHOT.war /tmp/student-${BUILD_ID}.war'
+                     sh 'aws s3 cp /tmp/student-${BUILD_ID}.war  s3://chorbucket-builder'   
+                }
+            }        
         }
     }
 }
