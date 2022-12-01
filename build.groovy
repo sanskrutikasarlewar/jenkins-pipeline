fdd588cddb7f4a149a2142b2f08ad9b2f0874c9a
anjali <anjali@gmail.com>
2022-12-01 13:17:43 +0530

    code commited 2

--- build.groovy
+++ build.groovy
@@ -11,5 +11,15 @@ pipeline {
                 sh 'mvn clean package' 
             }
         }
+        stage('artifact to s3-bucket') {
+            steps {
+                withAWS(credentials: 'mohit', region: 'us-west-2') {
+                sh 'sudo apt update -y'
+                sh 'sudo apt install awscli -y'
+                sh 'sudo s3 ls'
+                sh 'aws s3 mb s3://chorbucket-builder --region us-west-2'        
+            }
+            }
+        }
     }
 }
