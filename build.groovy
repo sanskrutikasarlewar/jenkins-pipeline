c878211e8c09277488533328e03a0b2c6bc1ab40
anjali <anjali@gmail.com>
2022-12-01 12:30:26 +0530

    build code commit

---
+++ build.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages {
+        stage('code-pulling') {
+            steps {
+                git credentialsId: 'github', url: 'git@github.com:mohit-decoder/student-ui.git'
+            }
+        }
+        stage('build-maven') {
+            steps {
+                sh 'maven clean package' 
+            }
+        }
+    }
+}
