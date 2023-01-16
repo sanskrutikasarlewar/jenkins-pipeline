f7c36a7f89e08bcd0ac1a0ca5af00bb26a717a61
sanskrutikasarlewar <62432724+sanskrutikasarlewar@users.noreply.github.com>
2023-01-16 14:57:32 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,5 +6,12 @@ pipeline {
                git branch: 'main', credentialsId: 'Sanskruti', url: 'git@github.com:sanskrutikasarlewar/jenkins-pipeline.git'
             }
         }
+        stage('build') {
+            steps {
+                sh 'sudo apt update -y'
+                sh 'sudo apt install maven -y'
+                sh 'mvn clean package'
+            }
+        }
     }
 }
