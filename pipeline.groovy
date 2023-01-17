969ca32b6e1de0143a734ee3d77d5882594a9faa
sanskrutikasarlewar <62432724+sanskrutikasarlewar@users.noreply.github.com>
2023-01-17 17:23:08 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,6 +4,19 @@ pipeline {
         stage('code-pull') {
             steps {
                git branch: 'main', credentialsId: 'Sanskruti', url: 'git@github.com:sanskrutikasarlewar/jenkins-pipeline.git'
+               sh 'echo "Hello"'
+            
+            }
+        }
+        stage('Build') {
+            steps {
+               echo 'Build'
+            
+            }
+        }
+        stage('test') {
+            steps {
+               echo 'Test'
             }
         }
     }
