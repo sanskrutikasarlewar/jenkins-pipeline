46a350b537fb15566e7fb5eeb67276c39cd1e386
sanskruti <you@example.com>
2023-01-20 15:47:35 +0530

    new

--- tomcat.groovy
+++ tomcat.groovy
@@ -3,18 +3,19 @@ pipeline {
         label 'tomcat-server'
     }
     stages{
-        stage('code-pull'){
+        stage('code-pull') {
             steps {
                 git credentialsId: 'sk', url: 'https://github.com/sanskrutikasarlewar/student-ui.git'
                 sh 'sudo apt update -y'
                 sh 'sudo apt install git -y'
             }
         }
-        stage('code-build'){
+        stage('code-build') {
             steps {
-                sh 'sudo apt update -y'
-                sh 'sudo apt install maven -y'
-                sh 'sudo mvn clean package'
+                sh ''' sudo apt update -y
+                sudo apt install maven -y
+                 sudo mvn clean package
+                 '''
             }
         } 
     }   
