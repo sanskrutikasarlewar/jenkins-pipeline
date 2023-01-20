909b286310bbdb2beafada4d1da5b2845a7211f9
sanskruti <you@example.com>
2023-01-20 17:21:27 +0530

    new

--- tomcat.groovy
+++ tomcat.groovy
@@ -11,7 +11,8 @@ pipeline {
         }
         stage('code-build') {
             steps {
-                sh '''sudo apt-get update -y
+                sh '''
+                sudo apt-get update -y
                 sudo apt-get install maven -y
                 mvn clean package
                  '''
