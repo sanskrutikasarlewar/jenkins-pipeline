082e7a964409ec7b402d2c484daf0486ce75cb32
sanskruti <you@example.com>
2023-01-21 19:48:29 +0530

    cli update

--- tomcat.groovy
+++ tomcat.groovy
@@ -16,7 +16,7 @@ pipeline {
                 sudo apt-get update -y
                 sudo apt-get install maven -y
                 mvn clean package
-                 '''
+                '''
             }
         }
         stage('copy-s3'){
@@ -27,7 +27,7 @@ pipeline {
                 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                 unzip awscliv2.zip
                 sudo ./aws/install
-                #aws s3 cp s3://student-app-artifact1
+                aws s3 cp **/*.war s3://student-app-artifact1
                 '''
             }
         }
