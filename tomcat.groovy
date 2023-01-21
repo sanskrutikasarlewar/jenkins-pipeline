3cad3fe9e5d168819b87f9d16977d92c6ab536e3
sanskruti <you@example.com>
2023-01-21 22:43:19 +0530

    new

--- tomcat.groovy
+++ tomcat.groovy
@@ -36,23 +36,32 @@ pipeline {
         stage('tomcat-server-build'){
             steps{
                 withCredentials([sshUserPrivateKey(credentialsId: 'bigboss-agent', keyFileVariable: 'tomcat', usernameVariable: 'ubuntu')]) {
-                    sh'''
-                    ssh -i ${tomcat} -o StrictHostKeyChecking=no ${ubuntu}@13.54.95.223
-                    sudo apt-get update -y
+                    sh 
+                    '''
+                    #ssh -i ${tomcat} -o StrictHostKeyChecking=no ${ubuntu}@13.54.95.223
+                    #sudo apt-get update -y
                     #sudo apt-get install unzip git -y
                     #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     #unzip awscliv2.zip
                     #sudo ./aws/install
-                    sudo apt-get install openjdk-11-jdk -y
-                    aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
+                    #sudo apt-get install openjdk-11-jdk -y
+                    #aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
                     #sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.zip
                     #sudo unzip apache-tomcat-8.5.85.zip /mnt/
-                    sudo chmod +x /mnt/apache-tomcat-8.5.85/bin/*
-                    sudo sh /mnt/apache-tomcat-8.5.85/bin/shutdown.sh
-                    sudo cp -rv student-${BUILD_ID}.war studentapp.war
-                    sudo cp -rv studentapp.war /mnt/apache-tomcat-8.5.85/webapp/
-                    sudo sh /mnt/apache-tomcat-8.5.85/bin/startup.sh
+                    #sudo chmod +x /mnt/apache-tomcat-8.5.85/bin/*
+                    #sudo sh /mnt/apache-tomcat-8.5.85/bin/shutdown.sh
+                    #sudo cp -rv student-${BUILD_ID}.war studentapp.war
+                    #sudo cp -rv studentapp.war /mnt/apache-tomcat-8.5.85/webapp/
+                    #sudo sh /mnt/apache-tomcat-8.5.85/bin/startup.sh
                     '''
+
+                    sh 'sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.70/bin/apache-tomcat-9.0.70.zip -P /opt/'
+                    sh 'sudo unzip /opt/apache-tomcat-9.0.70.zip -C .'
+                    aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
+                    sudo cp -rv student-${BUILD_ID}.war studentapp.war
+                    sudo cp -rv studentapp.war /opt/apache-tomcat-9.0.70/webapps/
+                    sudo sh /opt/apache-tomcat-9.0.70/bin/startup.sh
+                
                 }
             }
         }
