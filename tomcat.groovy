20d884f0238308087990439b8e934e19982516d5
sanskruti <you@example.com>
2023-01-21 20:59:18 +0530

    cli update

--- tomcat.groovy
+++ tomcat.groovy
@@ -39,7 +39,7 @@ pipeline {
                     sh'''
                     ssh -i ${tomcat} -o StrictHostKeyChecking=no ${ubuntu}@13.211.141.151
                     sudo apt-get update -y
-                    sudo apt-get install unzip git -y
+                    #sudo apt-get install unzip git -y
                     #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     #unzip awscliv2.zip
                     #sudo ./aws/install
