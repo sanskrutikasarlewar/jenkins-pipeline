200e06532d0c4541a0cf9ea58e189f4ab1db2239
sanskruti <you@example.com>
2023-01-21 22:35:08 +0530

    new

--- tomcat.groovy
+++ tomcat.groovy
@@ -50,7 +50,7 @@ pipeline {
                     sudo chmod +x /mnt/apache-tomcat-8.5.85/bin/*
                     sudo sh /mnt/apache-tomcat-8.5.85/bin/shutdown.sh
                     sudo cp -rv student-${BUILD_ID}.war studentapp.war
-                    sudo cp -rv studentapp.war /mnt/apache-tomcat-8.5.85/webapp
+                    sudo cp -rv studentapp.war /mnt/apache-tomcat-8.5.85/webapp/
                     sudo sh /mnt/apache-tomcat-8.5.85/bin/startup.sh
                     '''
                 }
