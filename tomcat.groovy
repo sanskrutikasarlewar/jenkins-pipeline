51a1540ea87e6871db2f6576b221f35804dacf0b
sanskruti <you@example.com>
2023-01-21 22:56:33 +0530

    new

--- tomcat.groovy
+++ tomcat.groovy
@@ -45,8 +45,8 @@ pipeline {
                     #sudo ./aws/install
                     sudo apt-get install openjdk-11-jdk -y
                     aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
-                    sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.zip
-                    sudo unzip apache-tomcat-8.5.85.zip /mnt/
+                    #sudo wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.zip
+                    #sudo unzip apache-tomcat-8.5.85.zip /mnt/
                     sudo chmod +x /mnt/apache-tomcat-8.5.85/bin/*
                     sudo sh /mnt/apache-tomcat-8.5.85/bin/shutdown.sh
                     sudo cp -rv student-${BUILD_ID}.war studentapp.war
