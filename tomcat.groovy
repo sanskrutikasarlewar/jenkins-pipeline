26cedf2a3e14f38cdb3bc6e2c0d9a911b35c19fa
sanskruti <you@example.com>
2023-01-22 00:17:16 +0530

    cli update

--- tomcat.groovy
+++ tomcat.groovy
@@ -45,6 +45,7 @@ pipeline {
                     #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
                     #unzip awscliv2.zip
                     #sudo ./aws/install
+                    $PWD
                     aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
                     curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
                     sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
