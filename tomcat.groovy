694c2d8f9e8b68e7c22bbe305713d339b4c0030c
sanskruti <you@example.com>
2023-01-27 12:14:14 +0530

    changes

--- tomcat.groovy
+++ tomcat.groovy
@@ -28,8 +28,8 @@ pipeline {
                 #unzip awscliv2.zip
                 #sudo ./aws/install
                 #aws s3 ls
-                sudo mv /home/ubuntu/workspace/pipe/target/studentapp-2.2-SNAPSHOT.war /tmp/student-${BUILD_ID}.war
-                aws s3 cp /tmp/student-${BUILD_ID}.war s3://student-app-artifact1 
+                sudo mv /home/ubuntu/workspace/Tomcat-pipe/target/studentapp-2.2-SNAPSHOT.war /tmp/student-${BUILD_ID}.war
+                aws s3 cp /tmp/student-${BUILD_ID}.war s3://student-app-artifact121
                 '''
             }
         }
@@ -47,7 +47,7 @@ pipeline {
                     #unzip awscliv2.zip
                     #sudo ./aws/install
                     $PWD
-                    aws s3 cp s3://student-app-artifact1/student-${BUILD_ID}.war .
+                    aws s3 cp s3://student-app-artifact121/student-${BUILD_ID}.war .
                     curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.85/bin/apache-tomcat-8.5.85.tar.gz
                     sudo tar -xvf apache-tomcat-8.5.85.tar.gz -C /opt/
                     sudo sh /opt/apache-tomcat-8.5.85/bin/shutdown.sh
